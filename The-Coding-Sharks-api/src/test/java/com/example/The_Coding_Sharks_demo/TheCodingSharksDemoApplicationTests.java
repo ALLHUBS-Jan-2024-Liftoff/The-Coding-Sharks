8814024904a7c3ba13bdaa01a90924bf185508ac
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-06-29 22:34:40 -0500

    added api

---
+++ The-Coding-Sharks-api/src/test/java/com/example/The_Coding_Sharks_demo/TheCodingSharksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.The_Coding_Sharks_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCodingSharksDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
