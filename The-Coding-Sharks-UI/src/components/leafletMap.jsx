c1427d154d8ce5d4b4f99502662e016ee2e22ba1
nicolepeak <colecpeak@gmail.com>
2024-07-17 21:50:48 -0500

    API basics linked to UI

---
+++ The-Coding-Sharks-UI/src/components/leafletMap.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer, Marker} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon, marker, popup } from "leaflet";
+
+const LeafletMap = () => {
+    const mapRef = useRef(null);
+    
+    //using placeholder locations which will actually come from the Database later
+    const markers = [
+        {
+            geocode: [38.6516758442689, -90.25925641100909],
+            popUp: "LaunchCode HQ"
+        },
+        {
+            geocode: [38.6377976058171, -90.28211059392594], 
+            popUp: "STL Zoo"
+        },
+        {
+            geocode: [38.75801729531715, -90.35330027799324],
+            popUp: "STL International Airport"
+        }
+    ];
+  
+    return ( 
+        <MapContainer center={[38.6270, -90.1994]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          
+          {markers.map(marker => (
+            <Marker position ={marker.geocode}>
+            </Marker>
+          ))}
+
+        </MapContainer>
+    );
+  };
+  
+  export default LeafletMap;
--- The-Coding-Sharks-UI/src/main.jsx
+++ The-Coding-Sharks-UI/src/main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
