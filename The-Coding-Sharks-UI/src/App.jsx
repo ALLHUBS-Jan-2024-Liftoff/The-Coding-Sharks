cd4ca1625e4659945f0ce64655dd00a3412f1451
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-08-18 13:35:30 -0500

    added AllTrips and integrated on switch logic to App.jsx

--- The-Coding-Sharks-UI/src/App.jsx
+++ The-Coding-Sharks-UI/src/App.jsx
@@ -9,19 +9,46 @@ import Packlist from "./components/packList";
 import RandomDestination from "./components/randomDestination";
 import Home from "./components/home";
 import Trip from "./components/trip";
+import AllTrips from "./components/AllTrips";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/randomDestination" element={<RandomDestination />} />
         <Route path="/packList" element={<Packlist />} />
-        <Route path="/trip" element={<Trip />} />
+        <Route path="/trip/:tripId" element={<Trip />} /> {/* Assuming you pass tripId as a parameter */}
+        <Route path="/allTrips" element={<AllTrips />} />
+        <Route path="/login" element={<Login onFormSwitch={toggleForm} />} />
+        <Route path="/register" element={<Register onFormSwitch={toggleForm} />} />
       </Routes>
+      <div>
+        {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
+      </div>
     </div>
   );
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/randomDestination" element={<RandomDestination />} />
+//         <Route path="/packList" element={<Packlist />} />
+//         <Route path="/trip" element={<Trip />} />
+//       </Routes>
+//     </div>
+//   );
+// }
 
 //I have shelly's home page commented out to set up routing.
 
