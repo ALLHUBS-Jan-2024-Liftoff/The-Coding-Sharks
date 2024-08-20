c1e953bcf3013edb59a84be427e493c40b4431c7
nicolepeak <colecpeak@gmail.com>
2024-08-19 19:39:48 -0500

    addToTrip added to randomDestination

--- The-Coding-Sharks-UI/src/App.jsx
+++ The-Coding-Sharks-UI/src/App.jsx
@@ -9,6 +9,7 @@ import Packlist from "./components/packList";
 import RandomDestination from "./components/randomDestination";
 import Home from "./components/home";
 import Trip from "./components/trip";
+import Test from "./components/test";
 
 import AllTrips from "./components/AllTrips";
 
@@ -24,6 +25,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/randomDestination" element={<RandomDestination />} />
         <Route path="/packList" element={<Packlist />} />
         <Route path="/trip/:tripId" element={<Trip />} /> {/* Assuming you pass tripId as a parameter */}
@@ -32,41 +34,9 @@ function App() {
         <Route path="/register" element={<Register onFormSwitch={toggleForm} />} />
       </Routes>
       <div>
-        {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
+        {/* {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />} */}
       </div>
     </div>
   );
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Routes>
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/randomDestination" element={<RandomDestination />} />
-//         <Route path="/packList" element={<Packlist />} />
-//         <Route path="/trip" element={<Trip />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-//I have shelly's home page commented out to set up routing.
-
-// function App() {
-//   const [currentForm, setCurrentForm] = useState('login');
-
-//   const toggleForm = (formName) => {
-//     setCurrentForm(formName);
-//   }
-
-//   return (
-//     <div className="App">
-//         {
-//           currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-//         }
-//       </div>
-//   )
-// }
 export default App;
